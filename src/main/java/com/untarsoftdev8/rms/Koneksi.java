a5628c57f9f35224c382de208abc5fccf8cc4033
jmslord <jmseffendy@gmail.com>
2022-05-07 20:52:23 +0700

    add koneksi

---
+++ src/main/java/com/untarsoftdev8/rms/Koneksi.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.untarsoftdev8.rms;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Jmslord
+ */
+public class Koneksi {
+    
+   private static Connection koneksi;
+    
+    public static Connection getKoneksi(){
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/penjualan";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Berhasil");
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    
+    public static void main(String[] args) {
+        getKoneksi();
+    }
+    
+}
